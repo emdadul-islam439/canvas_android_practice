6360e81fb95c5c7b4a455fd5d5ffcaea27cefeaa
emdadul_islam <emdadulislam439@gmail.com>
2021-04-28 16:00:06 +0600

    created MiniPaintActivity.kt [codelab-2 project]

---
+++ app/src/main/java/com/example/canvasandroidpractice/MiniPaintActivity.kt
@@ -0,0 +1,11 @@
+package com.example.canvasandroidpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MiniPaintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mini_paint)
+    }
+}
