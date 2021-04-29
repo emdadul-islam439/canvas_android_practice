52438f03d30c6ace15f0dd71269d186dcdbb39c9
emdadul_islam <emdadulislam439@gmail.com>
2021-04-29 11:51:41 +0600

    draw the first rectangle... (need to understand it immediately)

--- app/src/main/java/com/example/canvasandroidpractice/ClippingExampleActivity.kt
+++ app/src/main/java/com/example/canvasandroidpractice/ClippingExampleActivity.kt
@@ -2,10 +2,11 @@ package com.example.canvasandroidpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.canvasandroidpractice.clipping_example_classes.ClippedView
 
 class ClippingExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_clipping_example)
+        setContentView(ClippedView(this))
     }
 }
